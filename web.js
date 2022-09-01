8b9f16992b8715c2f4ff7430122ad505c0743825
aelims <dofla35689@gmail.com>
2022-09-01 16:18:09 +0900

    test9

--- web.js
+++ web.js
@@ -11,12 +11,12 @@
 // })
 
 const express = require('express');
-const router = require('/home/hosting_users/bcd1031/apps/bcd1031_meetgoga/routes');
-const sequelize = require('/home/hosting_users/bcd1031/apps/bcd1031_meetgoga/models').sequelize;
-const methodOverride = require('/home/hosting_users/bcd1031/apps/bcd1031_meetgoga/node_modules/method-override/index');
+const router = require('/home/hosting_users/bcd1031/apps/meetgoga/routes');
+const sequelize = require('/home/hosting_users/bcd1031/apps/meetgoga/models').sequelize;
+const methodOverride = require('/home/hosting_users/bcd1031/apps/meetgoga/node_modules/method-override/index');
 const http = require('http');
-const cors = require('/home/hosting_users/bcd1031/apps/bcd1031_meetgoga/node_modules/cors/index');
-const ejs = require("/home/hosting_users/bcd1031/apps/bcd1031_meetgoga/node_modules/ejs/index");
+const cors = require('/home/hosting_users/bcd1031/apps/meetgoga/node_modules/cors/libs/index');
+const ejs = require("ejs");
 
 var url = require('url');
 var fs = require('fs');
