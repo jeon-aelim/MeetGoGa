3444c470150c461b1d09f2b5674b03617db3bfb5
aelims <dofla35689@gmail.com>
2022-09-03 19:59:34 +0900

    test11

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const guideRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/guide/guideRoute');
-const guideGradeRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/guide_grade/guideGradeRoute');
-const guideMarkRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/guide_mark/guideMarkRoute');
-const guideReviewRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/guide_review/guideReviewRoute');
-const imgRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/img/imgRoute');
-const memberRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/member/memberRoute');
-const packageRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/package/packageRoute');
-const packageGradeRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/package_grade/packageGradeRoute');
-const packageMarkRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/package_mark/packageMarkRoute');
-const packageReviewRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/package_review/packageReviewRoute');
-const reviewCommentRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/reviewComment/reviewCommentRoute');
-const reviewLikeRoute = require('home/hosting_users/bcd1031/apps/bcd1031_meetgoga/reviewLike/reviewLikeRoute');
+const guideRoute = require('./guide/guideRoute');
+const guideGradeRoute = require('./guide_grade/guideGradeRoute');
+const guideMarkRoute = require('./guide_mark/guideMarkRoute');
+const guideReviewRoute = require('./guide_review/guideReviewRoute');
+const imgRoute = require('./img/imgRoute');
+const memberRoute = require('./member/memberRoute');
+const packageRoute = require('./package/packageRoute');
+const packageGradeRoute = require('./package_grade/packageGradeRoute');
+const packageMarkRoute = require('./package_mark/packageMarkRoute');
+const packageReviewRoute = require('./package_review/packageReviewRoute');
+const reviewCommentRoute = require('./reviewComment/reviewCommentRoute');
+const reviewLikeRoute = require('./reviewLike/reviewLikeRoute');
 
 router.use('/guide', guideRoute);
 router.use('/guideGrade', guideGradeRoute);
