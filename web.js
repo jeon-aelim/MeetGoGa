9bbe17aa77a1d6c68e0e341af0426621d4017f7e
aelims <dofla35689@gmail.com>
2022-09-15 11:38:58 +0900

    cmt28

--- web.js
+++ web.js
@@ -48,7 +48,7 @@ app.get('/test', (req, res) => {
   res.render('/home/hosting_users/bcd1031/apps/bcd1031_meetgoga/views/meetgo.html')
 });
 app.get('/main', (req, res) => {
-  res.render(path.join(__dirname, '/home/hosting_users/bcd1031/apps/bcd1031_meetgoga/build/index.html'))
+  res.render(path.join(__dirname, '/home/hosting_users/bcd1031/apps/bcd1031_meetgoga/views/build/index.html'))
 });
 app.use(express.static("images"));
 
